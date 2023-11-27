5f231172a67738424bb3729169dce8734bc3f6ec
razzivofficial <razzivofficial@gmail.com>
2023-11-27 11:59:01 +0530

    updated-code

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,29 @@
-
-
-
 /* Slider js by tushar start */
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
 
 let currentIndex = 0;
 
 function nextSlide() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    updateSlider();
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSlider();
 }
 
 function prevSlide() {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    updateSlider();
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateSlider();
 }
 
 function updateSlider() {
-    const translateValue = -currentIndex * 100 + '%';
-    slider.style.transform = 'translateX(' + translateValue + ')';
+  const translateValue = -currentIndex * 100 + "%";
+  slider.style.transform = "translateX(" + translateValue + ")";
 }
 
 setInterval(nextSlide, 10000);
 
-document.addEventListener('DOMContentLoaded', function() {
-    var container = document.querySelector('.l1');
-    container.style.display = 'block';
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var container = document.querySelector(".l1");
+  container.style.display = "block";
+});
 
-  /* Slider js by tushar End */
-
-
+/* Slider js by tushar End */
