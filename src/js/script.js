63432e1a663f334196f5dbfbbf6ae05faf4ef248
coolascool <aniket.787068@gmail.com>
2023-11-28 14:47:28 +0530

    update navbar

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,59 @@
+// navbar started ----->
+
+// small screen
+var card = document.getElementById("card");
+var maincontainer = document.getElementById("main-container");
+function openmenu(){
+    card.style.right = "0";
+}
+function closemenu(){
+    card.style.right = "-275px";
+}
+
+// sign in
+function togglePopupsignin() {
+    var popup = document.getElementById("signin-popup");
+    var overlay = document.querySelector(".overlaysignin");
+
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+        overlay.style.display = "none";
+    } else {
+        popup.style.display = "block";
+        overlay.style.display = "block";
+    }
+}
+function signIn() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    togglePopup();
+}
+
+// sign up
+function togglePopupsignup() {
+    var popup = document.getElementById("signup-popup");
+    var overlay = document.querySelector(".overlaysignup");
+
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+        overlay.style.display = "none";
+    } else {
+        popup.style.display = "block";
+        overlay.style.display = "block";
+    }
+}
+function signUp() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var mobile = document.getElementById("mobile").value;
+    var password = document.getElementById("password").value;
+    alert("Name: " + name + "\nEmail: " + email + "\nMobile: " + mobile + "\nPassword: " + password);
+    togglePopup();
+    return false;
+}
+
+// -----> navbar ended
+
 /* Slider js by tushar start */
 const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slide");
